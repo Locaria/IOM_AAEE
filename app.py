68be735f49302b34f4f8be8cfd5ccfc2b009a373
Yngrid Figlioli Locaria <138659569+YngridWFiglioli@users.noreply.github.com>
2024-04-10 15:16:55 +0200

    app streamlit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+st.title('Search in Google Sheets')
+
+word_search = st.text_input("Keyword", "Type a keyword...")
+country_search = st.text_input("Country", "Enter a country code (e.g., PT, UK, ES, IT)...")
+
+
+if st.button('Search'):
+    scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/creds.json', scope)
+    client = gspread.authorize(creds)
+    spreadsheet_id = 'your_spreadsheet_id'
+    spreadsheet = client.open_by_key(spreadsheet_id)
+    sheet = spreadsheet.sheet1
+    lines = sheet.get_all_records()
+
+    search_result = []
+
+    for line in lines:
+        if line["Target Country"] == country_search.upper() and word_search.lower() in line["Translation"].lower():
+            search_result.append(line["Keyword"])
+
+    if search_result:
+        st.write("Word(s) found corresponding to the search:")
+        for word in search_result:
+            st.write(word)
+    else:
+        st.write(f"No match found for '{word_search}' in the country {country_search}.")
