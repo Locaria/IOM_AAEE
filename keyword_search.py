586ba1adb95104a011b8eb02a5bcfcecdc7b9158
Yngrid Figlioli Locaria <138659569+YngridWFiglioli@users.noreply.github.com>
2024-05-24 09:25:32 +0200

    set creds path

--- keyword_search.py
+++ keyword_search.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import filedialog, simpledialog, messagebox
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
 
 def upload_excel():
     filepath = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
@@ -16,10 +17,15 @@ def download_excel(data):
         return
     data.to_excel(filepath, index=False)
 
-def search_keywords(dataframe, country):
+def get_google_sheets_credentials():
+    creds_file_path = os.getenv('GOOGLE_SHEETS_CREDS')
+    if not creds_file_path:
+        raise EnvironmentError("Google Sheets credentials file path not set in environment variable 'GOOGLE_SHEETS_CREDS'.")
     scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        r"C:/Users/yngrid.figlioli/Desktop/AAEE/IOM/plunetpulls-ca50ccd56cf0.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_file_path, scope)
+    return creds
+
+def search_keywords(dataframe, country, creds):
     client = gspread.authorize(creds)
     spreadsheet_id = '1fkzvhb7al-GFajtjRRy3b93vCDdlARBmCTGDrxm0KVY'
     spreadsheet = client.open_by_key(spreadsheet_id)
@@ -43,21 +49,34 @@ def search_keywords(dataframe, country):
 
 def main():
     root = tk.Tk()
-    root.withdraw()  # Esconde a janela principal
+    root.withdraw()  # Hide the main window
 
     country = simpledialog.askstring("Input", "Please enter the country:", parent=root)
     if not country:
         root.destroy()
         return
 
-    df = upload_excel()
-    if df is None:
+    try:
+        creds = get_google_sheets_credentials()
+    except EnvironmentError as e:
+        messagebox.showerror("Error", str(e))
         root.destroy()
         return
 
-    updated_df = search_keywords(df, country)
-    download_excel(updated_df)
+    df = upload_excel()
+    if df is None:
+        messagebox.showerror("Error", "No file selected.")
+        root.destroy()
+        return
 
+    try:
+        updated_df = search_keywords(df, country, creds)
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
+        root.destroy()
+        return
+
+    download_excel(updated_df)
     messagebox.showinfo("Done", "The operation was completed successfully.", parent=root)
     root.destroy()
 
